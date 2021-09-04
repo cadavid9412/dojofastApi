5fec57578ba33c5ec471cf7ea54df7f099aac828
CadavidR <sebastian.cadavid9412@udea.edu.co>
2021-09-04 11:23:18 -0500

    crud user

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.db.config import server
+from app.routers.api import router
+
+app = FastAPI(title="Dojo FastAPI")
+
+
+@app.on_event("startup")
+async def startup_event():
+    server(app)
